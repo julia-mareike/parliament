4c53840966a913a8a5c0793d0ec2334035ed8686
Julia Niall <jnmareike@gmail.com>
2020-08-22 15:29:02 +1200

    Updates

--- src/utils/index.js
+++
@@ -1,5 +0,0 @@
-export { getCoordinates } from './get-coordinates'
-export { calculateVotes, getSeats } from './calculate-seats'
-export { getSeatAllocations } from './get-seat-allocations'
-export { sortSeats } from './sort-seats'
-export { styles } from './styles'
