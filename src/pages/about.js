b3c7c5f56d4919ef3ea597beac5bc290b36a433e
Julia Niall <jnmareike@gmail.com>
2020-08-24 13:25:31 +1200

    docs(about): add prelim notes on the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,13 +9,11 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Parliament" />
-      <div className='mainWrapper rowWrapper' style={{ flexDirection: 'column' }}>
-        <div>
-          <a href='https://thespinoff.co.nz/politics/14-09-2017/mmp-maths-how-party-vote-percentages-become-seats-in-parliament/'>what? -> words</a>
-        </div>
-        <div>
-          <a href='https://www.youtube.com/watch?v=pljoleVHFug'>huh? -> video</a>
-        </div>
+      <div className='mainWrapper' style={{ flexDirection: 'column' }}>
+        <p><a href='https://www.youtube.com/watch?v=pljoleVHFug'>MMP: a video introduction</a></p>
+        <p>party colours sourced from wikipedia: <a href='https://en.wikipedia.org/wiki/List_of_political_parties_in_New_Zealand'>list of political parties in new zealand</a></p>
+        <p>it's like the official elections nz <a href='https://elections.nz/stats-and-research/mmp-seat-allocation-calculator/'>MMP seat allocation calculator</a>, but prettier!</p>
+        <p>how do party vote percentages become seats in parliament? <a href='https://thespinoff.co.nz/politics/14-09-2017/mmp-maths-how-party-vote-percentages-become-seats-in-parliament/'>the spinoff tells you how</a>.</p>
       </div>
     </Layout>
   )
