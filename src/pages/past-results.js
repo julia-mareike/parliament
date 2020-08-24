ae0ab6150eaf4d496e48222c95fbe9888334b4e0
mxbialostocki <matt.bialostocki@gmail.com>
2020-08-24 16:10:11 +1200

    style(pastres): center year buttons

--- src/pages/past-results.js
+++ src/pages/past-results.js
@@ -7,7 +7,7 @@ import { Inputs } from '../components/inputs'
 import { getCoordinates, getSeatAllocations } from '../utils'
 import { years } from '../utils/data'
 
-import { Button } from '@material-ui/core'
+import { Grid, Button } from '@material-ui/core'
 
 import './index.css'
 
@@ -19,28 +19,30 @@ const PastResults = () => {
   return (
     <Layout>
       <SEO title="Parliament" />
-      <div className='mainWrapper'>
-        <div className='wrapper parliament'>
+      <Grid container spacing={4} direction='row' justify="center" style={{ width: '80%', margin: '0 auto' }}>
+
+        <Grid item xs={12} sm={6}>
           <Parliament coordinates={coordinates} seats={seats} year={activeYear} />
-          <div className='rowWrapper buttons'>
-            {years.map(year => (
-              <Button
-                name={year}
-                key={year}
-                onClick={() => {
-                  setSeats(getSeatAllocations(year))
-                  setActiveYear(year)
-                }}
-              >
-                {year}
-              </Button>
-            ))}
-          </div>
-        </div>
-        <div className='wrapper'>
+        </Grid>
+        <Grid item xs={12} sm={6}>
           <Inputs year={activeYear} setSeats={setSeats} />
-        </div>
-      </div>
+        </Grid>
+        <Grid container item justify="space-between" xs={10}>
+          {years.map(year => (
+            <Button
+              name={year}
+              key={year}
+              onClick={() => {
+                setSeats(getSeatAllocations(year))
+                setActiveYear(year)
+              }}
+            >
+              {year}
+            </Button>
+          ))}
+        </Grid>
+
+      </Grid>
     </Layout>
   )
 }
