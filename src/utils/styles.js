1f75f2b182c8bdfb02570190c46f09f6c666670b
Julia Niall <jnmareike@gmail.com>
2020-08-24 13:26:00 +1200

    feat(style): update on-brand party colours

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -1,15 +1,27 @@
-const roughness = 1
+const roughness = 0.8
+const fillStyle = 'solid'
 
 export const styles = {
-  Labour: { fill: 'rgba(255, 0, 0, 0.7)', fillStyle: 'solid', roughness },
-  NZFirst: { fill: 'rgba(0, 0, 0, 0.7)', fillStyle: 'solid', roughness },
-  Greens: { fill: 'rgba(0, 128, 0, 0.7)', fillStyle: 'solid', roughness },
-  ACT: { fill: 'rgba(255, 255, 0, 0.7)', fillStyle: 'solid', roughness: roughness + 1 },
-  National: { fill: 'rgba(0, 0, 255, 0.7)', fillStyle: 'solid', roughness },
-  Maori: { fill: 'rgba(255, 0, 0, 1)', fillStyle: 'cross-hatch', roughness },
-  UnitedFuture: { fill: 'rgba(128, 0, 128, 0.7)', fillStyle: 'solid', roughness },
-  Mana: { fill: 'rgba(0, 0, 0, 0.7)', fillStyle: 'cross-hatch', roughness },
-  JAProgressive: { fill: 'rgba(0, 0, 255, 0.7)', fillStyle: 'cross-hatch', roughness },
-  UnitedNZ: { fill: 'rgba(255, 255, 0, 0.7)', fillStyle: 'cross-hatch', roughness: roughness },
-  Alliance: { fill: 'rgba(0, 128, 0, 0.7)', fillStyle: 'cross-hatch', roughness }
+  ACT: { fill: 'rgba(253, 228, 1, 1)', fillStyle, roughness },
+  Advance: { fill: 'rgba(25, 135, 209, 1)', fillStyle, roughness },
+  Alliance: { fill: 'rgba(51, 204, 102, 1)', fillStyle: 'cross-hatch', roughness },
+  Greens: { fill: 'rgba(9, 129, 55, 1)', fillStyle, roughness },
+  Heartland: { fill: 'rgba(122, 187, 75, 1)', fillStyle, roughness },
+  JAProgressive: { fill: 'rgba(158, 158, 158, 1)', fillStyle, roughness },
+  Labour: { fill: 'rgba(255, 0, 0, 1)', fillStyle, roughness },
+  Legalise: { fill: 'rgba(51, 204, 51, 1)', fillStyle, roughness },
+  Mana: { fill: 'rgba(119, 8, 8, 1)', fillStyle: 'cross-hatch', roughness },
+  Māori: { fill: 'rgba(178, 0, 26, 1)', fillStyle, roughness },
+  National: { fill: 'rgba(0, 82, 159, 1)', fillStyle, roughness },
+  NewConservative: { fill: 'rgba(73, 173, 234, 1)', fillStyle, roughness },
+  NZFirst: { fill: 'rgba(0, 0, 0, 1)', fillStyle, roughness },
+  OneParty: { fill: 'rgba(249, 209, 26, 1)', fillStyle, roughness },
+  Outdoors: { fill: 'rgba(0, 128, 0, 1)', fillStyle, roughness },
+  SocialCredit: { fill: 'rgba(203, 232, 0, 1)', fillStyle, roughness },
+  SustainableNZ: { fill: 'rgba(0, 128, 128, 1)', fillStyle, roughness },
+  TEAParty: { fill: 'rgba(90, 39, 108, 1)', fillStyle, roughness },
+  TOP: { fill: 'rgba(50, 218, 195, 1)', fillStyle, roughness },
+  UnitedFuture: { fill: 'rgba(80, 21, 87, 1)', fillStyle, roughness },
+  UnitedNZ: { fill: 'rgba(102, 51, 119, 1)', fillStyle, roughness },
+  VisionNZ: { fill: 'rgba(66, 49, 33, 1)', fillStyle, roughness },
 }
