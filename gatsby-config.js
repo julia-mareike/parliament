493cd2ed5e054ce07b1cfcd7498ea6a1010cf5fd
Julia Niall <jnmareike@gmail.com>
2020-08-26 20:45:32 +1200

    feat(favicon): add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,13 +56,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `parliament`,
+        short_name: `parliament`,
         start_url: `/`,
         background_color: `#000`,
         theme_color: `#000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/svg/beehive-favicon.svg`, // This path is relative to the root of the site.
       },
     },
     'custom-mui-theme'
