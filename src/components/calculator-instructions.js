5562aa995c400709e44074bfb05f9085407998e1
Julia Niall <jnmareike@gmail.com>
2020-08-27 13:40:43 +1200

    feat(results): update calculator instructions

--- src/components/calculator-instructions.js
+++ src/components/calculator-instructions.js
@@ -18,7 +18,7 @@ export const CalculatorInstructions = () => {
       <tspan x={xBase + 20} dy={'1.2em'}>
         of the party votes
       </tspan>
-      <tspan x={xBase + 7} dy={'1.2em'}>
+      <tspan x={xBase + 12} dy={'1.2em'}>
         between the parties
       </tspan>
       <tspan x={xBase} dy={'1.2em'}>
@@ -33,17 +33,20 @@ export const CalculatorInstructions = () => {
       <tspan x={xBase + 80} dy={'1.2em'}>
         get.
       </tspan>
-      <tspan x={xBase + 27} dy={'2.4em'}>
-        electorate seats
-      </tspan>
-      <tspan x={xBase + 40} dy={'1.2em'}>
-        mostly affect
+      <tspan x={xBase + 12} dy={'2.4em'}>
+        electorate seats are
       </tspan>
       <tspan x={xBase + 20} dy={'1.2em'}>
-        parties with fewer
+        optional & mostly
       </tspan>
-      <tspan x={xBase + 7} dy={'1.2em'}>
-        than 5% of the votes.
+      <tspan x={xBase + 12} dy={'1.2em'}>
+        affect parties with
+      </tspan>
+      <tspan x={xBase + 40} dy={'1.2em'}>
+        fewer than 5%
+      </tspan>
+      <tspan x={xBase + 40} dy={'1.2em'}>
+        of the votes.
       </tspan>
     </text>
   )
--- src/components/party-legend.js
+++ src/components/party-legend.js
@@ -14,7 +14,6 @@ const PartyLegend = ({ seats }) => {
   let textBase = 205
   let seatsArray = []
   let overhang = 0
-  console.log({ seats })
   if (seats) {
     for (let party in seats) {
       if (seats[party].overhang) {
